c1bfb75664183723aa03b9bca8a9920018aed1f6
George Schneeloch <bostonbusmap@gmail.com>
2012-02-16 23:00:47 -0500

    adding config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Senescribe::Application.config.secret_token = 'e5324c3fddbc96749703fca8026adaf6278c6a5a88a6e072ee5edc31cd277a2eb651fdb08959af8c1afec67bd8b6b535a5dde6e5ce24ea07776ace467f2772c2'
